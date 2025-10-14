41e2b366d106f4801f4bd12883886a3eda7ef79c
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2025-10-14 13:47:49 +0200

    Fix port fot application manager

--- app-manager/start_tts.sh
+++ app-manager/start_tts.sh
@@ -3,7 +3,7 @@ mkdir -p /workspace/logs
 export PYTHONUNBUFFERED=1
 export HF_HOME="/workspace"
 # Set port for the React UI
-export PORT=3006
+export REACT_UI_PORT=3006
 VENV_PATH=$(cat /workspace/TTS-WebUI/venv_path)
 source ${VENV_PATH}/bin/activate
 cd /workspace/TTS-WebUI
